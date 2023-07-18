213eaef1419e3a875843ba347f816e12232ae9c8
Nikita Khubaev <khubaev.n@yandex.ru>
2023-07-18 19:41:25 +0400

    initial commit for  level-1 branch

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: ['_id'],
+      },
+    ],
+  },
+};
