0f6a1501ccb7615c62380c7d7ae799aa88b006ad
Nikita Khubaev <khubaev.n@yandex.ru>
2023-07-28 16:20:05 +0400

    создал модель user

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/unauthorized-err');
+
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Никита',
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Некорректный формат email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password') // this - это модель User
+    .then((user) => {
+      // не нашелся - отклоняем промис
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Неправильные почта или пароль');
+          }
+
+          return user; // user доступен
+        });
+    });
+};
+
+const user = mongoose.model('user', userSchema);
+
+module.exports = user;
